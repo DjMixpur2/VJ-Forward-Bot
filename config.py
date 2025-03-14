b82756ccde9c8061768932d25e469413a99b79c4
DjMixpur2 <djmixpur2@gmail.com>
2025-03-14 21:33:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    API_ID = int(environ.get("API_ID", "21344245"))
+    API_HASH = environ.get("API_HASH", "e72de40c666f1664f847a79f97dd3882")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7980904477:AAHQOHBwPES7dkDrgHPF40_leghWYQbIoFo") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
     DATABASE_URI = environ.get("DATABASE_URI", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    BOT_OWNER = int(environ.get("BOT_OWNER", "7585839477"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
